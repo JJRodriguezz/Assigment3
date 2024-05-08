9a14d8cd09ff0ec0605ef4804f9662f24f01e6ad
JJRodriguezz <jjrodrigur@eafit.edu.co>
2024-05-07 23:21:03 -0500

    Se ha agregado la función JJRR

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main{
-    
+
+    public static void main(String[] args)  {
+    JJRR();
+    }
+
+    public static void JJRR() {
+        System.out.println("Assigment 3 by Juan José Rodríguez");
+    }
 }
